87987c0fe1b5f4ac0ffb5bc5a896fd09ef916d3f
Patrick-1963 <pleguet@free.fr>
2026-03-05 14:51:09 +0100

    Update service worker to version 8

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agenda-pro-v7';
+const CACHE_NAME = 'agenda-pro-v8';
 const urlsToCache = [
   'agenda.html',
   'manifest.json',
@@ -94,4 +94,5 @@ self.addEventListener('notificationclick', event => {
       if (clients.openWindow) return clients.openWindow('agenda.html');
     })
   );
+
 });
